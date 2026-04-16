b725817cd248f768dd497caf7acc63e91e192121
Saurish <saurishsum10@outlook.com>
2026-04-15 22:32:46 -0700

    Update to GET users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,8 +34,8 @@ const users = {
   ],
 };
 
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user["id"] === id);
 };
 
 app.use(express.json());
@@ -44,14 +44,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
   } else {
-    res.send(users);
+    res.send(result);
   }
 });
 
